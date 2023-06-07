de1cb19729fb65bcaf51df081510f246e71803fa
Serhan miah <101991709+SerhanMiah@users.noreply.github.com>
2023-06-07 22:32:34 +0100

    redoing the homepage for eventify

--- client/src/components/Home/EventHomePage.js
+++ client/src/components/Home/EventHomePage.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, Form, Pagination } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
 
 const EventHomePage = () => {
     const [events, setEvents] = useState([]);
+    const [search, setSearch] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [eventsPerPage] = useState(10); // You can adjust this number
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('http://localhost:5245/api/events');
+                const response = await axios.get(`http://localhost:5245/api/events?page=${currentPage}&search=${search}`);
                 setEvents(response.data);
             } catch (error) {
                 console.error('Error fetching data from server', error);
@@ -18,7 +20,26 @@ const EventHomePage = () => {
         };
 
         fetchData();
-    }, []);
+    }, [currentPage, search]);
+
+    const onChangeSearch = e => {
+        setSearch(e.target.value);
+        setCurrentPage(1); // Reset to first page when search changes
+    };
+
+    const totalEvents = events.length;
+    const indexOfLastEvent = currentPage * eventsPerPage;
+    const indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
+    const currentEvents = events.slice(indexOfFirstEvent, indexOfLastEvent);
+
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+
+    // Get page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(totalEvents / eventsPerPage); i++) {
+        pageNumbers.push(i);
+    }
 
     return (
         <Container fluid>
@@ -28,7 +49,17 @@ const EventHomePage = () => {
                 </Col>
             </Row>
             <Row>
-                {events.map(event => (
+                <Col xs={12}>
+                    <Form.Control
+                        type="text"
+                        placeholder="Search for event..."
+                        value={search}
+                        onChange={onChangeSearch}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                {currentEvents.map(event => (
                     <Col xs={12} md={6} lg={4} key={event.id} className="mb-4">
                         <Card>
                             <Card.Img variant="top" src={event.imageUrl} />
@@ -46,6 +77,17 @@ const EventHomePage = () => {
                         </Card>
                     </Col>
                 ))}
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <Pagination>
+                        {pageNumbers.map(number => (
+                            <Pagination.Item key={number} active={number === currentPage} onClick={() => paginate(number)}>
+                                {number}
+                            </Pagination.Item>
+                        ))}
+                    </Pagination>
+                </Col>
             </Row>
         </Container>
     );
