3d15dd4c276738d3057b339959f6ba8cfbef9296
Serhan miah <101991709+SerhanMiah@users.noreply.github.com>
2023-06-09 11:20:24 +0100

    creating buffer to set, get, decode, user auth in the nav bar components

--- client/src/components/Navigation/NavigationBar.js
+++ client/src/components/Navigation/NavigationBar.js
@@ -4,8 +4,20 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import '../../styles/navigation.css';
+import { useNavigate } from "react-router-dom";
+import { userIsAuthenticated } from '../helpers/auth';
+
 
 const NavigationBar = () => {
+    const navigate = useNavigate();
+
+    const handleLogOut = () => {
+        window.localStorage.removeItem('local-user-Token')
+        navigate('/')
+    };
+
+    const isAuthenticated = userIsAuthenticated();
+
     return (
         <div className="navigation">
             <div className="navbar-top">
@@ -20,19 +32,27 @@ const NavigationBar = () => {
                                 {/* Add more navigation links as needed */}
                             </Nav>
                             <Nav className="ml-auto">
-                            <LinkContainer to="/register">
-                                <Nav.Link><FontAwesomeIcon icon={faUser} /> Register</Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to="/login">
-                                <Nav.Link><FontAwesomeIcon icon={faUser} /> Login</Nav.Link>
-                            </LinkContainer>
-                            <NavDropdown title="Account" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.3">Logout</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
+                            { userIsAuthenticated
+                                ?
+                                <>
+                                    <NavDropdown title="Account" id="basic-nav-dropdown">
+                                        <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={handleLogOut}>Logout</NavDropdown.Item>
+                                    </NavDropdown>
+                                </>
+                                :
+                                <>
+                                    <LinkContainer to="/register">
+                                        <Nav.Link><FontAwesomeIcon icon={faUser} /> Register</Nav.Link>
+                                    </LinkContainer>
+                                    <LinkContainer to="/login">
+                                        <Nav.Link><FontAwesomeIcon icon={faUser} /> Login</Nav.Link>
+                                    </LinkContainer>
+                                </>
+                            }
+                            </Nav>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
